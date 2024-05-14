910d67b0bba60cde460013f594e3dd60d5b5528f
Evgeny Bayandin <5087081@gmail.com>
2024-05-14 08:09:38 +0300

    second commit

---
+++ src/main/java/org/my/example/App.java
@@ -0,0 +1,13 @@
+package org.my.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Java!" );
+    }
+}
